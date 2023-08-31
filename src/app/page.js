ea51c6969c822ecf6dca90b664df9aa3db90dc8f
Max Alejandro Rivera Higuera <maxalejandrorh@hotmail.com>
2023-08-31 00:11:33 -0700

    Function_coordinates

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+"use client"; // This is a client component 👈🏽
+
 import App from 'src/app/app.jsx'
 import 'src/app/app.jsx';
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
+    <div className="flex min-h-screen items-center justify-center">
       <App/>
     </div>
   )
